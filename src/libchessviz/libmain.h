45c858dbd568fee6c26a4be85788ae1ca59b75ee
Igor_panasevich <igor_panasevich@mail.ru>
2021-05-13 01:07:09 +0700

    delete excess code

--- src/libchessviz/libmain.h
+++ src/libchessviz/libmain.h
@@ -15,21 +15,7 @@ bool is_figure(char c);
 bool is_ah(char c);
 bool is_18(char c);
 int is_valid_string(string& s, char color[8][8]);
-bool is_correct_move(
+void move(
         string s,
-        char c,
         char field[8][8],
         char color[8][8]); // bool w_cast, bool b_cast
-char get_enemy(char c);
-vector<pair<int, int>>
-attacks(int I, int J, char field[8][8], char color[8][8]);
-bool king_check(char c, char field[8][8], char color[8][8]);
-bool try_long_castling(
-        char c, char field[8][8], char color[8][8], bool w_cast, bool b_cast);
-bool try_short_castling(
-        char c, char field[8][8], char color[8][8], bool w_cast, bool b_cast);
-
-const string IN_PROGRESS = "in progress";
-const string WIN_WHITE = "win_white";
-const string WIN_BLACK = "win_black";
-const string DRAW = "draw";
